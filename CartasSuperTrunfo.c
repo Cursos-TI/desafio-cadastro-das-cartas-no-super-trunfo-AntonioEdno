2024c748e464e5f66535f70b72cd86e1d6035a3a
AntonioEdno <edno.df.algar@gmail.com>
2025-03-10 21:54:12 +0000

    Primeira etapa finalizada do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,85 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Estrutura de uma Carta
+typedef struct {
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char Estado;                 // Estado (A a H)
+    char CodigoCarta[50];        // Código da carta (ex: A01, B03)
+    char Cidade[50];             // Nome da CIdade
+    int Populacao;               // População da Cidade
+    float Area;                  // Area da Cidade (em km²)
+    float Pib;                   // PIB da cidade (em bilhões de reais)
+    int Num_pontos_turisticos;   // Número de pontos turísticos na cidade
+   
+} Carta;
+
+ // Função para cadastrar as cartas
+
+void inserirDadosCarta(Carta *carta) {
+
+ // Solicitando o estado   
+    printf("Digite o Estado  (Uma letra de 'A' a 'H'): "); 
+    scanf(" %c", &carta->Estado); // Um caractere representando o estado, que deve ser uma letra de 'A' a 'H'.
+
+ // Solicitando o código da carta    
+    printf("Digite o Codigo da carta (ex: A01, B03): "); //Uma string que representa o código da carta (ex: A01, B03).
+    scanf("%s", &carta->CodigoCarta);
+
+ // Solicitando o nome da cidade
+    printf("Digite o Nome da Cidade: "); //Uma string que contém o nome da cidade.
+    scanf("%s", &carta->Cidade);
+
+ // Solicitando a população   
+    printf("Digite a população: "); //Um número inteiro representando a população da cidade.
+    scanf("%f", &carta->Populacao);
+  
+ // Solicitando a área   
+    printf("Digite a área (em km²): "); //A área da cidade em quilômetros quadrados (tipo float).
+    scanf("%f", &carta->Area);
+  
+ // Solicitando o PIB   
+    printf("Informe o PIB (bilhoes de reais): "); //O PIB da cidade em bilhões de reais (tipo float).
+    scanf("%f", &carta->Pib);
+  
+ // Solicitando o número de pontos turísticos   
+    printf("Digite o número de pontos turísticos: ");//O número de pontos turísticos na cidade (tipo int).
+    scanf("%d", &carta->Num_pontos_turisticos);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    }
 
-    return 0;
+// Função para exibir os dados das cartas
+
+void exibirCarta(Carta carta) {
+     printf("Estado: %c\n", carta.Estado);
+     printf("Código: %s\n", carta.CodigoCarta);
+     printf("Cidade: %s\n", carta.Cidade);
+     printf("População: %d\n", carta.Populacao);
+     printf("Área: %.2f km²\n", carta.Area);
+     printf("PIB: %.2f bilhoes de reais\n", carta.Pib);
+     printf("Número de pontos turísticos: %d\n", carta.Num_pontos_turisticos);
+   
+}
+
+int main(){
+
+    Carta carta1, carta2;
+
+ // Inserir dados para a primeira carta
+    printf("\nInforme os dados da primeira carta:\n");
+    inserirDadosCarta(&carta1); //Solicita os dados ao usuário para preencher a estrutura Carta
+
+ // Inserir dados para a segunda carta
+    printf("\nInforme os dados da segunda carta:\n");
+    inserirDadosCarta(&carta2); //Solicita os dados ao usuário para preencher a estrutura Carta
+
+ // Exibir os dados das duas cartas
+    printf("\nCarta 1: \n");// Função printf para imprimir
+    exibirCarta(carta1); //Exibe os dados de uma carta formatados de forma legível
+
+    printf("\nCarta 2: \n");
+    exibirCarta(carta2); //Exibe os dados de uma carta formatados de forma legível
+
+
+return 0;
+
 }
